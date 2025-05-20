d56b1d2104c49b0b38d34071f5db80124f28b782
aiden <an010719@naver.com>
2025-05-20 15:54:06 +0900

    HotelReg & HotelOwner layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Footer from "./components/Footer";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
 import Mybookings from "./pages/Mybookings";
+import HotelReg from "./components/HotelReg";
+import Layout from "./pages/hotelOwner/Layout";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -12,12 +14,14 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <NavBar />}
+      {false && <HotelReg />}
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-bookings" element={<Mybookings />} />
+          <Route path="/owner" element={<Layout />} />
         </Routes>
       </div>
       <Footer />
